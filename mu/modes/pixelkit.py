070abfcb84cfd166c5a21c5d75de7da7b3484e39
Murilo Polese <murilopolese@gmail.com>
2018-08-07 22:52:49 +0100

    Flashing MicroPython for ESP32 on Pixel Kit

--- mu/modes/pixelkit.py
+++ mu/modes/pixelkit.py
@@ -23,6 +23,8 @@ import sys
 import os.path
 import logging
 import semver
+import subprocess
+from time import sleep
 from tokenize import TokenError
 from mu.logic import HOME_DIRECTORY
 from mu.contrib import uflash
@@ -30,6 +32,7 @@ from mu.contrib import pixelfs as pixelfs
 from mu.modes.api import MICROBIT_APIS, SHARED_APIS
 from mu.modes.base import MicroPythonMode
 from PyQt5.QtCore import QObject, QThread, pyqtSignal, QTimer
+from PyQt5.QtWidgets import QMessageBox
 
 # We can run without nudatus
 can_minify = False
@@ -42,29 +45,65 @@ class DeviceFlasher(QThread):
     """
     # Emitted when flashing the Pixel Kit fails for any reason.
     on_flash_fail = pyqtSignal(str)
+    on_step = pyqtSignal(str)
 
-    def __init__(self, paths_to_microbits, python_script, path_to_runtime):
-        """
-        The paths_to_microbits should be a list containing filesystem paths to
-        attached Pixel Kits to flash. The python_script should be the text of
-        the script to flash onto the device. The path_to_runtime should be the
-        path of the hex file for the MicroPython runtime to use. If the
-        path_to_runtime is None, the default MicroPython runtime is used by
-        default.
-        """
+    def __init__(self, port):
         QThread.__init__(self)
-        self.paths_to_microbits = paths_to_microbits
-        self.python_script = python_script
-        self.path_to_runtime = path_to_runtime
+        self.port = port
+
+    def erase_flash(self):
+        erase_flash = 'python mu/contrib/esptool.py --baud 921600 --port {0} erase_flash'.format(self.port)
+        p = subprocess.Popen(erase_flash, stdout=subprocess.PIPE, shell=True)
+        result = b''
+        while True:
+            out = p.stdout.readline()
+            if out == b'' and p.poll() != None:
+                break
+            if out != b'':
+                logger.info(out)
+                result += out
+                self.on_step.emit("{0}: {1}".format(_("Erasing flash"), out.decode('utf-8')))
+        return result
+
+    def write_flash(self, fname=''):
+        write_flash = 'python mu/contrib/esptool.py --baud 921600 --port {0} write_flash 0x1000 {1}'.format(self.port, fname)
+        p = subprocess.Popen(write_flash, stdout=subprocess.PIPE, shell=True)
+        result = b''
+        line = b''
+        while True:
+            out = p.stdout.read(1)
+            if out == b'' and p.poll() != None:
+                break
+            if out != b'':
+                if out == b'\r' or out == b'\n':
+                    logger.info(line)
+                    self.on_step.emit("{0}: {1}".format(_("Writing flash"), line.decode('utf-8')))
+                    result += line
+                    line = b''
+                else:
+                    line += out
+        return result
+
+    def download_firmware(self):
+        import tempfile
+        import urllib.request
+        # Check if there is internet connection
+        url = 'http://micropython.org/resources/firmware/esp32-20180511-v1.9.4.bin'
+        f = tempfile.NamedTemporaryFile()
+        f.close()
+        logger.info("Downloading firmware to {0}".format(f.name))
+        self.on_step.emit(_("Downloading firmware."))
+        urllib.request.urlretrieve(url, f.name)
+        return f.name
 
     def run(self):
         """
         Flash the device.
         """
         try:
-            uflash.flash(paths_to_microbits=self.paths_to_microbits,
-                         python_script=self.python_script,
-                         path_to_runtime=self.path_to_runtime)
+            filename = self.download_firmware()
+            erase_result = self.erase_flash()
+            write_result = self.write_flash(filename)
         except Exception as ex:
             # Catch everything so Mu can recover from all of the wide variety
             # of possible exceptions that could happen at this point.
@@ -156,7 +195,6 @@ class FileManager(QObject):
             logger.error(ex)
             self.on_delete_fail.emit(microbit_filename)
 
-
 class PixelKitMode(MicroPythonMode):
     """
     Represents the functionality required by the Kano Pixel Kit mode.
@@ -171,10 +209,6 @@ class PixelKitMode(MicroPythonMode):
     valid_boards = [
         (0x0403, 0x6015),  # Kano Pixel Kit USB VID, PID
     ]
-
-    valid_serial_numbers = [9900, 9901]  # Serial numbers of supported boards.
-
-    python_script = ''
 
     def actions(self):
         """
@@ -233,14 +267,23 @@ class PixelKitMode(MicroPythonMode):
         """
         if not self.repl:
             self.toggle_repl(self)
-        self.stop()
         serial = self.view.serial
         if serial and self.view.current_tab and self.view.current_tab.text():
             code = self.view.current_tab.text()
             self.enterRawRepl(serial)
-            # write code to serial
             serial.write(bytes(code, 'ascii'))
+            sleep(0.01)
             self.exitRawRepl(serial)
+
+    def enterRawRepl(self, serial):
+        serial.write(b'\x01')
+        sleep(0.01)
+
+    def exitRawRepl(self, serial):
+        serial.write(b'\x04') # CTRL-D
+        sleep(0.01)
+        serial.write(b'\x02') # CTRL-B
+        sleep(0.01)
 
     def stop(self):
         """
@@ -252,7 +295,55 @@ class PixelKitMode(MicroPythonMode):
             self.view.serial.write(b'\x03') # CTRL-C
 
     def flash(self):
-        pass
+        from time import sleep
+        message = _("Flash your Pixel Kit with MicroPython.")
+        information = _("Make sure you have internet connection and don't "
+                        "disconnect your device during the process. It "
+                        "might take a minute or two but you will only need"
+                        "to do it once.")
+        if self.view.show_confirmation(message, information) != QMessageBox.Cancel:
+            port, serial = pixelfs.find_pixelkit()
+            if port:
+                self.set_buttons(mpflash=False, mpfiles=False, run=False, stop=False, repl=False)
+                self.flash_thread = DeviceFlasher(port)
+                self.flash_thread.finished.connect(self.flash_finished)
+                self.flash_thread.on_flash_fail.connect(self.flash_failed)
+                self.flash_thread.on_step.connect(self.on_step)
+                self.flash_thread.start()
+            else:
+                self.flash_failed('No Pixel Kit was found.')
+
+
+    def flash_finished(self):
+        self.set_buttons(mpflash=True, mpfiles=True, run=True, stop=True, repl=True)
+        self.editor.show_status_message(_('Pixel Kit was flashed. Have fun!'))
+        message = _("Pixel Kit was flashed. Have fun!")
+        information = _("Your Pixel Kit now has MicroPython on it. If you want "
+                        "to revert it to the original firmware, please "
+                        "download the Kano Code App and follow the onboarding "
+                        "process.")
+        self.view.show_message(message, information, 'Warning')
+        logger.info('Flash finished.')
+        self.flash_thread = None
+        self.flash_timer = None
+
+    def flash_failed(self, error):
+        self.set_buttons(mpflash=True, mpfiles=True, run=True, stop=True, repl=True)
+        logger.info('Flash failed.')
+        logger.error(error)
+        message = _("There was a problem flashing the Pixel Kit.")
+        information = _("Please do not disconnect the device until flashing"
+                        " has completed. Please check the logs for more"
+                        " information.")
+        self.view.show_message(message, information, 'Warning')
+        self.editor.show_status_message(_('Pixel Kit could not be flashed. Please restart the Pixel Kit and try again.'))
+        if self.flash_timer:
+            self.flash_timer.stop()
+            self.flash_timer = None
+        self.flash_thread = None
+
+    def on_step(self, message):
+        self.editor.show_status_message(message)
 
     def toggle_repl(self, event):
         """
